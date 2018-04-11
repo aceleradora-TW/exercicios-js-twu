b8246232db4d7e94652b745766ca042a31cbcea5
alineabich <aline.abich@acad.pucrs.br>
2018-04-11 09:40:03 -0300

    querocafe

---
+++ querocafeAline_lindice.js
@@ -0,0 +1,14 @@
+
+
+function queroCafe(mascada,precos) {
+  let arrAux =[]
+      for (let i = 0; i < precos.length; i++) {
+        if(precos [i] <= mascada) {
+          console.log(precos[i])
+
+        }
+      }
+
+}
+
+queroCafe(3.14, [5.16, 2.12, 1.15, 3.11, 17.5])
